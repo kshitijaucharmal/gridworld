e17d08242516078f77673a7b2e41c7a1893e3b58
KshitijAucharmal <39955649+kshitijaucharmal@users.noreply.github.com>
2021-10-31 09:01:22 +0530

    argmax fix

--- sketch.js
+++ sketch.js
@@ -130,8 +130,8 @@ function Q_Learning(){
 }
 
 function argmax(arr){
-    let max = -10000;
-    let arg = -1;
+    let max = -Infinity;
+    let arg = 0;
     for(let i = 0; i < arr.length; i++){
         if(arr[i] > max){
             max = arr[i];
